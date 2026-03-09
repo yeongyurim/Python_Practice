28c4ca735600c2da89239bcee3c64fb7de5ef7d6
yeongyurim <zxc991019@naver.com>
2026-03-09 19:20:13 +0900

    2026-03-08

--- BaekJoon/2675.py
+++ BaekJoon/2675.py
@@ -1,3 +1,9 @@
-a = input().split()
-b = input()
-print(a,b)
+n = int(input())
+for i in range(n) :
+    r,s = input().split()
+    r = int(r)
+    p = ""
+    for idx in range(len(s)) :
+        for j in range(r) :
+            p += s[idx]
+    print(p)
