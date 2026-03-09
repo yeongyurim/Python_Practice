42d2bcf164b3aa3a3c747cddb8cd676df0aa7627
yeongyurim <zxc991019@naver.com>
2026-03-09 19:24:58 +0900

    2026-03-05(2)

---
+++ BaekJoon/10809.py
@@ -0,0 +1,4 @@
+arr1 = input()
+alphas = [-1 for i in range(26)]
+for i in alphas :
+    print(i)
---
+++ BaekJoon/2558.py
@@ -0,0 +1,3 @@
+listA = list(map(int, input().split()))
+for i in range(len(listA)) :
+    listA[i] = listA[i] + int(input())
