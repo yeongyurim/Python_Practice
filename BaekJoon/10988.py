7c1aa84cc88e97dd0422b22883d39a4c3aaffce5
yeongyurim <zxc991019@naver.com>
2026-04-23 12:59:45 +0900

    2026-04-23

--- BaekJoon/10988.py
+++ BaekJoon/10988.py
@@ -1,6 +1,13 @@
 word = input()
 wordStack = []
-for i in range(len(word)/2) :
-    wordStack.append(i)
-while(wordStack) :
-    
+mid = len(word)//2
+pal = True
+for i in range(mid) :
+    wordStack.append(word[i])
+if len(word) % 2 == 1 :
+    mid += 1    
+for i in range(mid, len(word)) :
+    if(word[i] != wordStack.pop()) :
+        pal = False
+        break
+print("1" if pal else "0")
